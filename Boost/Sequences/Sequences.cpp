3998c91b3a2592a956bf53bea71710d755c42571
meson.xin <meson_xin@nettrix.com.cn>
2024-06-22 16:14:18 +0800

    更新C++ Boost库学习的Demo部分目录结构，更新Sequence目录及其相关测试代码

---
+++ Boost/Sequences/Sequences.cpp
@@ -0,0 +1,28 @@
+#include <boost/archive/text_oarchive.hpp>
+#include <boost/archive/text_iarchive.hpp>
+#include <iostream>
+#include <fstream>
+
+void save()
+{
+    std::ofstream file("archive.txt");
+    boost::archive::text_oarchive oa(file);
+    int i = 1;
+    oa << i;
+    //std::cout << i << std::endl;
+}
+
+void load()
+{
+    std::ifstream file("archive.txt");
+    boost::archive::text_iarchive ia(file);
+    int i = 0;
+    ia >> i;
+    std::cout << i << std::endl;
+}
+
+int main()
+{
+    save();
+    load();
+}
